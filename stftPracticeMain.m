5eb9e0fb0835a1bed7017935a0b4a6e5d32764b2
shion0825 <61345739+shion0825@users.noreply.github.com>
2022-03-25 14:11:05 +0900

    Add files via upload

---
+++ stftPracticeMain.m
@@ -0,0 +1,12 @@
+% 5年生の短時間Fourier変換（short-time Fourier transform: STFT）実習
+clear; close all; clc;
+
+% 440Hz＋880Hzの信号（10秒，サンプリング周波数16kHz）を生成
+
+% STFT実装
+
+% パワースペクトログラムの表示
+
+% 上記ができたら自分の声をaudacityで録音してwavファイルを用意し，パワースペクトログラムで見てみる
+
+% Good luck!
