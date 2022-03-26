03cf79c3ed3f3ee87cdd6008c9e442a42d5bb868
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-03-26 20:55:39 +0900

    生成する信号、変数の名前、二重ループ文完了。 ０パッティングはわからん

--- STFT2.m
+++ STFT2.m
@@ -1,49 +1,11 @@
 clear;clc;
 
-info=audioinfo("10.wav")
-[data,Fs]=audioread("10.wav"); %Fsはサンプリングレート
+info=audioinfo("voice.wav")
+[data,Fs]=audioread("voice.wav");%Fsはサンプリングレート
 
-Fs
+window_length=10; %窓長(偶数)
+shift_length=window_length/2; %シフト長
 
-%data=[1,2,3,4,5,6,7,8,9,10,11].';
+[num_data,num_channel]=size(data);  %データの数,チャンネル数
 
-N=4; %窓長
-
-A=size(data);
-num=A(1,1);  %データの数
-chan=A(1,2); %チャンネルの数(mp3はチャンネルが1~3ある?)
-
-channel=0 %チャンネルの指定をする変数
-
-%ピッタリサイズの行列の横幅の決定
-% N + N/2*(yoko-1) >= num を満たす最小のyokoが正解
-yoko=ceil(2*(num-N)/N)+1; %ceilは切り上げ
-
-%Xはスペクトラムを格納予定
-X=zeros(N,yoko,chan);
-
-%データを行列に並べる
-for channel=1:chan
-    for i=1:yoko-1
-        for j=1:N
-            X(j,i,channel)=data(N/2*(i-1)+j,1);
-        end
-    end
-%最後の列だけ0が残るかもしれないので別ループで処理
-    i=1;
-    for j=N/2*(yoko-1)+1:num
-        X(i,yoko,channel)=data(j,1);
-        i=i+1;
-    end
-end
-%行列の各ベクトルに窓関数をかける
-hann(N);
-X=X.*hann(N);
-
-%FFT
-X=fft(X);
-
-%各要素のパワー見せつける
-X=abs(X);
-X=X.^2;
-
+channel=0; %チャンネルの指定をする変数
--- STFTpractice.m
+++ STFTpractice.m
@@ -1,6 +1,6 @@
 clear;clc;
 
-t=(0:0.02:1)';
+t=(0:0.05:1)';
 data=sin(2*pi*t);
 
 window_length=10; %窓長(偶数)
@@ -12,22 +12,21 @@ shift_length=window_length/2; %シフト長
 % window_length + shift_length*(num_row-1) >= num_dataを満たす最小のnum_rowが正解
 num_row=ceil((num_data-window_length)/shift_length)+1; %ceilは切り上げ
 
-%Xはスペクトラムを格納予定
+%格納予定行列
 spectrogram=zeros(window_length,num_row);
 
+%きれいに並べるためにデータの最後に都合のいい数だけ0を詰め込む
+data=[data;zeros(1,num_data-(shift_length*(num_row-1)+1))'];
+
 %データを行列に並べる
-for i=1:num_row-1
-    for j=1:window_length
-        spectrogram(j,i)=data(window_length/2*(i-1)+j,1);
-    end
+for i=1:num_row
+    spectrogram(:,i)=data(shift_length*(i-1)+1:shift_length*(i-1)+window_length,1);
 end
 
-%最後の列だけ0が残るかもしれないので別ループで処理
-i=1;
-for j=window_length/2*(num_row-1)+1:num_data
-    spectrogram(i,num_row)=data(j,1);
-    i=i+1;
-end
+fprintf("データを並べただけのやつ")
+spectrogram
+
+%---------------------------------------------------------------------------
 
 %行列の各ベクトルに窓関数をかける
 spectrogram=spectrogram.*hann(window_length);
@@ -38,4 +37,5 @@ spectrogram=fft(spectrogram);
 %各要素のパワー見せつける
 spectrogram=abs(spectrogram).^2;
 
+fprintf("でけたやつ")
 spectrogram
