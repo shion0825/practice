a066e3e3cfff2d2837ede0b85e8630fbeda21d31
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-03-26 18:37:52 +0900

    いったんコミット

--- STFTpractice.m
+++ STFTpractice.m
@@ -1,31 +1,31 @@
 clear;clc;
 
-data=(1:23)'
+t=(0:0.02:1)';
+data=sin(2*pi*t);
 
-N=4; %窓長
+windw_length=10; %窓長
 
-A=size(data);
-num_data=A(1,1);  %データの数
+[num_data,num_channel]=size(data);  %データの数,チャンネル数
 
 %ピッタリサイズの行列の横幅の決定
 % N + N/2*(num_row-1) >= num_data を満たす最小のnum_rowが正解
-num_row=ceil(2*(num_data-N)/N)+1; %ceilは切り上げ
+num_row=ceil(2*(num_data-windw_length)/windw_length)+1; %ceilは切り上げ
 
 %Xはスペクトラムを格納予定
-spectrum=zeros(N,num_row)
+spectrogram=zeros(windw_length,num_row);
 
 %データを行列に並べる
 for i=1:num_row-1
-    for j=1:N
-        spectrum(j,i)=data(N/2*(i-1)+j,1);
+    for j=1:windw_length
+        spectrogram(j,i)=data(windw_length/2*(i-1)+j,1);
     end
 end
 
 %最後の列だけ0が残るかもしれないので別ループで処理
 i=1;
-for j=N/2*(num_row-1)+1:num_data
-    spectrum(i,num_row)=data(j,1);
+for j=windw_length/2*(num_row-1)+1:num_data
+    spectrogram(i,num_row)=data(j,1);
     i=i+1;
 end
 
-spectrum
+spectrogram
