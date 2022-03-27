5570983a51dd3e5daa38d20fa0a965cccbcc14a5
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-03-27 10:36:48 +0900

    一旦指摘されたところを編集した。

--- STFTpractice.m
+++ STFTpractice.m
@@ -1,4 +1,4 @@
-clear;clc;
+clear;
 
 t=(0:0.05:1)';
 data=sin(2*pi*t);
@@ -12,30 +12,21 @@ shift_length=window_length/2; %シフト長
 % window_length + shift_length*(num_row-1) >= num_dataを満たす最小のnum_rowが正解
 num_row=ceil((num_data-window_length)/shift_length)+1; %ceilは切り上げ
 
-%格納予定行列
-spectrogram=zeros(window_length,num_row);
-
 %きれいに並べるためにデータの最後に都合のいい数だけ0を詰め込む
 data=[data;zeros(1,num_data-(shift_length*(num_row-1)+1))'];
 
-%データを行列に並べる
+%格納予定行列
+spectrogram=zeros(window_length,num_row);
+
+%窓関数かける、DFTする、パワー！、データを行列に並べる
+%一行でもいいが、速さを気にしていないので分けてもええやろと思った
 for i=1:num_row
-    spectrogram(:,i)=data(shift_length*(i-1)+1:shift_length*(i-1)+window_length,1);
+    work_vector=data(shift_length*(i-1)+1:shift_length*(i-1)+window_length,1);
+    work_vector=work_vector.*hann(window_length);   %窓関数かける
+    work_vector=fft(work_vector);                   %fftする
+    work_vector=20*log10(abs(work_vector));         %パワーとる
+    spectrogram(:,i)=work_vector;                   %行列に並べる
 end
 
-fprintf("データを並べただけのやつ")
-spectrogram
-
-%---------------------------------------------------------------------------
-
-%行列の各ベクトルに窓関数をかける
-spectrogram=spectrogram.*hann(window_length);
-
-%各ベクトルにFFT
-spectrogram=fft(spectrogram);
-
-%各要素のパワー見せつける
-spectrogram=abs(spectrogram).^2;
-
-fprintf("でけたやつ")
+fprintf("完成スペクトログラム")
 spectrogram
