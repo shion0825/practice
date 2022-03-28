b1015077a430efe19179bd61157f9cbed7cc5a6c
K-Karato <86594359+pipikouki@users.noreply.github.com>
2022-03-28 16:53:45 +0900

    指摘されたところの編集 先生が指定した条件、見やすいスペース、linspace、y軸反転、表示範囲指定

--- STFTpractice.m
+++ STFTpractice.m
@@ -1,60 +1,52 @@
 clear;clc;
 
 %サンプリング周波数
-Fs=44100;
+Fs = 16000;
 %表示時間
-time=1;
+time = 10;
+%表示させる周波数
+display_frequency = 1000;
 
 t=(0:1/Fs:time)';
-data=100*sin(2*pi*1000*t)+1*sin(4*pi*1000*t);
+data = 100*sin(2*pi*440*t)+1*sin(2*pi*880*t);
 
-window_length=2^10; %窓長(偶数)
-shift_length=window_length/2; %シフト長
+window_length = 2^10; %窓長(偶数)
+shift_length = window_length/2; %シフト長
 
-[num_data,num_channel]=size(data);  %データの数,チャンネル数
+[num_data,num_channel] = size(data);  %データの数,チャンネル数
 
 %ピッタリサイズの行列の横幅の決定
 % window_length + shift_length*(num_row-1) >= num_dataを満たす最小のnum_rowが正解
-num_row=ceil((num_data-window_length)/shift_length)+1; %ceilは切り上げ
+num_row = ceil((num_data-window_length)/shift_length)+1; %ceilは切り上げ
 
 %きれいに並べるためにデータの最後に都合のいい数だけ0を詰め込む
-data=[data;zeros(1,num_data-(shift_length*(num_row-1)+1))'];
+data = [data;zeros(1,num_data-(shift_length*(num_row-1)+1))'];
 
 %格納予定行列
-complex_matrix=zeros(window_length,num_row);
-amplitude_matrix=zeros(window_length,num_row);
-power_spectrogram_matrix=zeros(window_length,num_row);
+complex_matrix = zeros(window_length,num_row);
+amplitude_matrix = zeros(window_length,num_row);
+power_spectrogram_matrix = zeros(window_length,num_row);
 
 %窓関数かける、DFTする、パワー！、データを行列に並べる
 %一行でもいいが、速さを気にしていないので分けてもええやろと思った
-for i=1:num_row
-    work_vector=data(shift_length*(i-1)+1:shift_length*(i-1)+window_length,1);
-    work_vector=work_vector.*hann(window_length);   %窓関数かける
-    work_vector=fft(work_vector);                   %fftする
-    complex_matrix(:,i)=work_vector;
-    work_vector=abs(work_vector);                   %大きさみる
-    amplitude_matrix=work_vector;
-    work_vector=20*log10(work_vector);              %パワーとる
-    power_spectrogram_matrix(:,i)=work_vector;
+for i = 1:num_row
+    work_vector = data(shift_length*(i-1)+1:shift_length*(i-1)+window_length,1);
+    work_vector = work_vector.*hann(window_length); %窓関数かける
+    work_vector = fft(work_vector);                 %fftする
+    %complex_matrix(:,i) = work_vector;
+    work_vector = abs(work_vector);                 %大きさみる
+    %amplitude_matrix = work_vector;
+    work_vector = 20*log10(work_vector);            %パワーとる
+    power_spectrogram_matrix(:,i) = work_vector;
 end
 
-fprintf("完成パワースペクトログラム")
-x=1:num_row;
-y=1:window_length;
-z=power_spectrogram_matrix(y,x);
+x = linspace(1,time,num_row);
+y = linspace(1,Fs,window_length);
+imagesc(x,y,power_spectrogram_matrix);
 
-x=x*time/num_row;
-y=y*Fs/window_length;
-
-imagesc(x,y,z)
+%----------グラフの編集----------
+axis xy;
 xlabel("time");
 ylabel("frequsency");
-
-%-----------------------ここから逆変換--------------------------------
-rebuild_matrix=zeros(window_length,num_row);
-
-for i=1:num_row
-    work_vector=complex_matrix(:,i);
-    work_vector=ifft(work_vector);
-    rebuild_matrix=work_vector;
-end
+ylim([Fs/window_length,display_frequency])
+%------------------------------
